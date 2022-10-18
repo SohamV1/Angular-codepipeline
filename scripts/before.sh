d6f9a9f3eabba0b67c7f00d6f11a7671c9b195b6
Soham Valsangkar <sohamvs1999@gmail.com>
2022-10-18 19:00:49 +0530

    updated appspec

--- scripts/before.sh
+++ scripts/before.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 sudo apt update 
 sudo apt install nginx -y
-sudo rm -rf /var/www/html/index.html
+sudo rm -rf /var/www/html/index.nginx-debian.html
+sudo curl -sL https://deb.nodesource.com/setup_14.x | bash -
+sudo apt update
+sudo apt install nodejs -y
+
